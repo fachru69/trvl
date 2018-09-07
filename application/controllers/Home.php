abc5cbc141322e382010da4f6cdfa46e889a13c2
Fachrudin Shaleh <1506087@sttgarut.ac.id>
2018-09-07 10:51:56 +0700

    terjadi kesalahan syntax

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -27,21 +27,4 @@ class Home extends CI_Controller {
         $this->load->view('Home/Home');
         
 	}
-    public function detail_article()
-    {
-        $data = array(
-            'artikel' = array (
-                '1' = array(
-                    "judul" => "Gunung Rinjani",
-                    "post" => "Gunung Rinjani adalah gunung yang berlokasi di Pulau Lombok, Nusa Tenggara Barat. Gunung yang merupakan gunung berapi kedua tertinggi di Indonesia dengan ketinggian 3.726 m dpl serta terletak pada lintang 8º25' LS dan 116º28' BT ini merupakan gunung favorit bagi pendaki Indonesia karena keindahan pemandangannya. Gunung ini merupakan bagian dari Taman Nasional Gunung Rinjani yang memiliki luas sekitar 41.330 ha dan ini akan diusulkan penambahannya sehingga menjadi 76.000 ha ke arah barat dan timur. Secara administratif gunung ini berada dalam wilayah tiga kabupaten: Lombok Timur, Lombok Tengah dan Lombok Barat."
-                            ),
-                '2' = array(
-                        "judul" => "Lembah Balieum",
-                        "post" =>"Lembah Balieum adalah gunung yang berlokasi di Pulau Lombok, Nusa Tenggara Barat. Gunung yang merupakan gunung berapi kedua tertinggi di Indonesia dengan ketinggian 3.726 m dpl serta terletak pada lintang 8º25' LS dan 116º28' BT ini merupakan gunung favorit bagi pendaki Indonesia karena keindahan pemandangannya. Gunung ini merupakan bagian dari Taman Nasional Gunung Rinjani yang memiliki luas sekitar 41.330 ha dan ini akan diusulkan penambahannya sehingga menjadi 76.000 ha ke arah barat dan timur. Secara administratif gunung ini berada dalam wilayah tiga kabupaten: Lombok Timur, Lombok Tengah dan Lombok Barat."
-                            );
-                );
-            );
-        
-        $this->load->view('Home/detailArtikel', $data);
-    }
 }
