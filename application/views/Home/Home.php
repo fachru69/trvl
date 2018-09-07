d3d7792ae2dde0f8d98ac6b0102655170946d06d
Fachrudin Shaleh <1506087@sttgarut.ac.id>
2018-09-07 15:09:28 +0700

    Revert "fixed"
    This reverts commit 4b804b154f4677dbb65fb98189afcd0bc8c2148a.

--- application/views/Home/Home.php
+++ application/views/Home/Home.php
@@ -9,7 +9,7 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-    <link href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" integrity="sha384-wvfXpqpZZVQGK6TAh5PVlGOfQNHSoD2xbE+QkPxCAFlNEevoEH3Sl0sibVcOQVnN" crossorigin="anonymous">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/font-awesome.min.css')?>">
     <link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/css/style.css')?>">
     <script src="<?php echo base_url('assets/js/style.js')?>"></script>
 </head>
